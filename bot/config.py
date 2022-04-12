2c32a02ca7eab8b91f2ef2ffcdf0709035a8d087
Horlarwumhe <amachiever4real@gmail.com>
2022-04-12 17:13:49 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,8 @@
-BOT_TOKEN = ''
+
+import os
+
+# BOT_TOKEN should not be put here. 
+# Put it in environment
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DATABASE_NAME = 'botdata.db'
 USER_WARNED_TIME = 60*5
