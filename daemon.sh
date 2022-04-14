f25ecd231c4b67c977fbe727562a7a77fdebb6ec
Horlarwumhe <amachiever4real@gmail.com>
2022-04-14 15:05:04 +0100

    Create directory for log files

--- daemon.sh
+++ daemon.sh
@@ -1,5 +1,6 @@
 export PYTHONUNBUFFERED=1
 export BOT_TOKEN=""
 cd $HOME/word-ban-bot
+mkdir -p logs
 pip3 install -r requirements.txt
 python3 main.py
--- start.bash
+++ start.bash
@@ -1,4 +1,4 @@
+mkdir -p logs
 export BOT_TOKEN=""
-export USER_WARNED_TIME="300" # 5 mins
 pip3 install -r requirements.txt
 python3 main.py
