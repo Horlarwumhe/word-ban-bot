cca0742ef8443a46bda643b5fb84bd09cb437ded
Horlarwumhe <amachiever4real@gmail.com>
2022-04-14 18:02:24 +0100

    Use relative path to move the unit file.

--- boot.sh
+++ boot.sh
@@ -5,7 +5,9 @@ DAEMON_DIR=$HOME/.config/systemd/user.control
 mkdir -p $DAEMON_DIR
 
 #move the unit file to the diretory systemd will search
-cp $HOME/word-ban-bot/word-ban-bot.service $DAEMON_DIR/word-ban-bot.service
+cp word-ban-bot.service $DAEMON_DIR/word-ban-bot.service
+
+systemctl --user daemon-reload
 systemctl --user enable word-ban-bot
 systemctl --user stop word-ban-bot 
 systemctl --user start word-ban-bot 
