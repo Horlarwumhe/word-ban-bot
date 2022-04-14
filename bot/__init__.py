937adcda27ac5f4c8d835ef7eed210da69290826
Horlarwumhe <amachiever4real@gmail.com>
2022-04-14 15:20:32 +0100

    Log bot messages into a file and console.

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,16 +1,15 @@
 import logging
-
+from logging.handlers import TimedRotatingFileHandler
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-logger = logging.getLogger("bot")
 
-# logger = logging.getLogger('bot')
-# stream = logging.StreamHandler()
-# stream.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s:%(levelname)s: %(message)s',
-#                               datefmt='%d/%m/%Y %H:%M:%S %p')
-# stream.setFormatter(formatter)
-# logger.addHandler(stream)
-# logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('bot')
+stream = TimedRotatingFileHandler("logs/bot.log",when="D")
+stream.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(name)s %(asctime)s:%(levelname)s: %(message)s',
+                              datefmt='%d/%m/%Y %H:%M:%S %p')
+stream.setFormatter(formatter)
+logger.addHandler(stream)
+logger.setLevel(logging.DEBUG)
