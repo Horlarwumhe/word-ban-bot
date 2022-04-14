0aa2716ab6ed615795d756b7dbeb074f35585012
Horlarwumhe <amachiever4real@gmail.com>
2022-04-14 17:33:10 +0100

    Stop the bot service if is running before starting.

--- boot.sh
+++ boot.sh
@@ -7,4 +7,5 @@ mkdir -p $DAEMON_DIR
 #move the unit file to the diretory systemd will search
 cp $HOME/word-ban-bot/word-ban-bot.service $DAEMON_DIR/word-ban-bot.service
 systemctl --user enable word-ban-bot
+systemctl --user stop word-ban-bot 
 systemctl --user start word-ban-bot 
