36a95524a14c2c94801f6f38e451e62fbc278596
Horlarwumhe <amachiever4real@gmail.com>
2022-04-14 16:03:34 +0100

    Updated boot.sh file

--- boot.sh
+++ boot.sh
@@ -1,2 +1,10 @@
+
+
+DAEMON_DIR=$HOME/.config/systemd/user.control
+# create directory for user systemd files
+mkdir -p $DAEMON_DIR
+
+#move the unit file to the diretory systemd will search
+cp $HOME/word-ban-bot/word-ban-bot.service $DAEMON_DIR/word-ban-bot.service
 systemctl -user enable word-ban-bot
 systemcl --user start word-ban-bot 
