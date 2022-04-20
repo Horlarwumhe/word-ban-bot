c97031adfebb547d26d131b3ef3a9567e5a0a6d0
Horlarwumhe <amachiever4real@gmail.com>
2022-04-20 10:07:06 +0100

    Change default encoding for opening log files.

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,15 +1,19 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-
 logger = logging.getLogger('bot')
-stream = TimedRotatingFileHandler("logs/bot.log",when="D")
+stream = TimedRotatingFileHandler("logs/bot.log",
+                                  when="D",
+                                  encoding="utf-8",
+                                  errors="ignore")
 stream.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(name)s %(asctime)s:%(levelname)s: %(message)s',
-                              datefmt='%d/%m/%Y %H:%M:%S %p')
+formatter = logging.Formatter(
+    '%(name)s %(asctime)s:%(levelname)s: %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S %p')
 stream.setFormatter(formatter)
 logger.addHandler(stream)
 logger.setLevel(logging.DEBUG)
